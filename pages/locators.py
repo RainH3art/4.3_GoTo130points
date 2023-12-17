5ad440e89d6293b6080dfa5959b9c45c7e8b4745
rainh3art <yusovqa@gmail.com>
2023-12-17 19:15:54 +0300

    Чуток рефактора под задание шага 2 - ранее выполнял в ру. локализации

--- pages/locators.py
+++ pages/locators.py
@@ -12,8 +12,8 @@ class PromoPageLocators():
     BASKET_BUTTON = (By.CSS_SELECTOR, '[class="btn btn-lg btn-primary btn-add-to-basket"]')
     BOOK_NAME = (By.XPATH, '//div[@class="col-sm-6 product_main"]//h1')
     BOOK_PRICE = (By.XPATH, '//div[@class="col-sm-6 product_main"]//p[@class="price_color"]')
-    BOOK_IN_BASKET_NAME = (By.XPATH, "//div[contains(@class, 'alertinner') and contains(., 'был добавлен в вашу корзину')]//strong")
-    BOOK_PRICE_IN_BASKET = (By.XPATH, "//div[@class='alertinner ']//*[contains(text(), 'Стоимость корзины теперь составляет')]//strong")
+    BOOK_IN_BASKET_NAME = (By.XPATH, "//div[contains(@class, 'alertinner') and contains(., 'has been added to your basket')]//strong")
+    BOOK_PRICE_IN_BASKET = (By.XPATH, "//div[@class='alertinner ']//*[contains(text(), 'Your basket total is now')]//strong")
 
     
    
