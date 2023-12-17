a78423dbc804d05e63882a4ea1c9fb16d55e98c9
rainh3art <yusovqa@gmail.com>
2023-12-17 19:56:21 +0300

    Рефактор - добавил ассерты на наличие элементов

--- pages/locators.py
+++ pages/locators.py
@@ -9,11 +9,11 @@ class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#register_form")
     
 class PromoPageLocators():
-    BASKET_BUTTON = (By.CSS_SELECTOR, '[class="btn btn-lg btn-primary btn-add-to-basket"]')
-    BOOK_NAME = (By.XPATH, '//div[@class="col-sm-6 product_main"]//h1')
-    BOOK_PRICE = (By.XPATH, '//div[@class="col-sm-6 product_main"]//p[@class="price_color"]')
-    BOOK_IN_BASKET_NAME = (By.XPATH, "//div[contains(@class, 'alertinner') and contains(., 'has been added to your basket')]//strong")
-    BOOK_PRICE_IN_BASKET = (By.XPATH, "//div[@class='alertinner ']//*[contains(text(), 'Your basket total is now')]//strong")
+    BASKET_BUTTON = (By.CSS_SELECTOR, '.btn-add-to-basket')
+    BOOK_NAME = (By.CSS_SELECTOR, '.product_main h1')
+    BOOK_PRICE = (By.CSS_SELECTOR, 'p.price_color')
+    BOOK_IN_BASKET_NAME = (By.CSS_SELECTOR, ".alertinner strong")
+    BOOK_PRICE_IN_BASKET = (By.CSS_SELECTOR, ".alertinner p strong")
 
     
    
--- pages/product_page.py
+++ pages/product_page.py
@@ -5,26 +5,31 @@ from .locators import PromoPageLocators
 class ProductPage(BasePage): 
     
     def remember_book_name(self):
+        assert self.is_element_present(*PromoPageLocators.BOOK_NAME), "Название книги отсутствует"
         self.book_name_element = self.browser.find_element(*PromoPageLocators.BOOK_NAME)
-        self.book_name =  self.book_name_element.text
-
+        self.book_name = self.book_name_element.text
+        
     def remember_book_price(self):
+        assert self.is_element_present(*PromoPageLocators.BOOK_PRICE), "Цена книги отсутствует"
         self.book_price_element = self.browser.find_element(*PromoPageLocators.BOOK_PRICE)
         self.book_price =  self.book_price_element.text
         
     def click_on_basket_button(self):
+        assert self.is_element_present(*PromoPageLocators.BASKET_BUTTON), "Кнопка добавления в корзину отсутствует"
         basket_button = self.browser.find_element(*PromoPageLocators.BASKET_BUTTON)
         basket_button.click()
         
     def fact_book_in_basket_name(self):
+        assert self.is_element_present(*PromoPageLocators.BOOK_IN_BASKET_NAME), "Название книги в корзине отсутствует"
         book_in_basket_name_element = self.browser.find_element(*PromoPageLocators.BOOK_IN_BASKET_NAME)
         book_in_basket_name = book_in_basket_name_element.text
-        assert self.book_name in book_in_basket_name, 'Название книги в корзине отличается от добавленной'
+        assert self.book_name == book_in_basket_name, f'Название книги в корзине {book_in_basket_name} отличается от добавленной {self.book_name}'
         
     def fact_price_in_basket(self):
+        assert self.is_element_present(*PromoPageLocators.BOOK_PRICE_IN_BASKET), "Цена книги в корзине отсутствует"
         book_price_in_basket_element = self.browser.find_element(*PromoPageLocators.BOOK_PRICE_IN_BASKET)
         book_price_in_basket = book_price_in_basket_element.text
-        assert self.book_price in book_price_in_basket, 'Цена книги в корзине отличается от указанной при добавлении'
+        assert self.book_price == book_price_in_basket, f'Цена книги в корзине {book_price_in_basket} отличается от указанной при добавлении {self.book_price}'
 
 
         
