d58195b7828dabf03cb4cc1e193c95a3022fe5b2
rainh3art <yusovqa@gmail.com>
2023-12-17 23:28:57 +0300

    Рефакторинг

--- pages/product_page.py
+++ pages/product_page.py
@@ -2,15 +2,24 @@ from .base_page import BasePage
 from .locators import PromoPageLocators
 
 
-class ProductPage(BasePage): 
+class ProductPage(BasePage):
+    def add_product_to_basket(self):
+        self.on_page_book_name()
+        self.on_page_book_price()
+        self.click_on_basket_button()
+        self.solve_quiz_and_get_code()
+        self.fact_book_in_basket_name()
+        self.fact_price_in_basket()
+        self.should_be_name_the_same()
+        self.should_be_price_the_same()
     
-    def remember_book_name(self):
-        assert self.is_element_present(*PromoPageLocators.BOOK_NAME), "Название книги отсутствует"
+    def on_page_book_name(self):
+        assert self.is_element_present(*PromoPageLocators.BOOK_NAME), "Элемент название книги отсутствует"
         self.book_name_element = self.browser.find_element(*PromoPageLocators.BOOK_NAME)
         self.book_name = self.book_name_element.text
         
-    def remember_book_price(self):
-        assert self.is_element_present(*PromoPageLocators.BOOK_PRICE), "Цена книги отсутствует"
+    def on_page_book_price(self):
+        assert self.is_element_present(*PromoPageLocators.BOOK_PRICE), "Элемент цена книги отсутствует"
         self.book_price_element = self.browser.find_element(*PromoPageLocators.BOOK_PRICE)
         self.book_price =  self.book_price_element.text
         
@@ -21,15 +30,20 @@ class ProductPage(BasePage):
         
     def fact_book_in_basket_name(self):
         assert self.is_element_present(*PromoPageLocators.BOOK_IN_BASKET_NAME), "Название книги в корзине отсутствует"
-        book_in_basket_name_element = self.browser.find_element(*PromoPageLocators.BOOK_IN_BASKET_NAME)
-        book_in_basket_name = book_in_basket_name_element.text
-        assert self.book_name == book_in_basket_name, f'Название книги в корзине {book_in_basket_name} отличается от добавленной {self.book_name}'
+        self.book_in_basket_name_element = self.browser.find_element(*PromoPageLocators.BOOK_IN_BASKET_NAME)
+        self.book_in_basket_name = self.book_in_basket_name_element.text
         
     def fact_price_in_basket(self):
         assert self.is_element_present(*PromoPageLocators.BOOK_PRICE_IN_BASKET), "Цена книги в корзине отсутствует"
-        book_price_in_basket_element = self.browser.find_element(*PromoPageLocators.BOOK_PRICE_IN_BASKET)
-        book_price_in_basket = book_price_in_basket_element.text
-        assert self.book_price == book_price_in_basket, f'Цена книги в корзине {book_price_in_basket} отличается от указанной при добавлении {self.book_price}'
+        self.book_price_in_basket_element = self.browser.find_element(*PromoPageLocators.BOOK_PRICE_IN_BASKET)
+        self.book_price_in_basket = self.book_price_in_basket_element.text
+
+    def should_be_name_the_same(self):   
+        assert self.book_name == self.book_in_basket_name, f'Название книги в корзине {self.book_in_basket_name} отличается от добавленной {self.book_name}'     
+    
+    def should_be_price_the_same(self):
+        assert self.book_price == self.book_price_in_basket, f'Цена книги в корзине {self.book_price_in_basket} отличается от указанной при добавлении со страницы {self.book_price}'
+    
 
 
         
--- test_product_page.py
+++ test_product_page.py
@@ -16,12 +16,7 @@ def test_guest_can_add_product_to_basket(browser, link):
     link = f"{link}"
     page = ProductPage(browser, link)
     page.open()
-    page.remember_book_name()
-    page.remember_book_price()
-    page.click_on_basket_button()
-    page.solve_quiz_and_get_code()
-    page.fact_book_in_basket_name()
-    page.fact_price_in_basket()
+    page.add_product_to_basket()
     
     # pytest -v -s .\test_product_page.py
 
