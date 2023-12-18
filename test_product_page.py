2901f39a6380864b905278b41dd4ea96787c8151
rainh3art <yusovqa@gmail.com>
2023-12-18 12:22:43 +0300

    Переделал последние 3 теста под одну ссылку

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,5 @@
 from pages.product_page import ProductPage
 import pytest
-import time
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -18,26 +17,29 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.open()
     page.add_product_to_basket()
     
-    # pytest -v -s .\test_product_page.py
 @pytest.mark.xfail    
-def test_guest_cant_see_success_message_after_adding_product_to_basket(browser, link):
-    link = f"{link}"
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
     page.should_not_be_success_message()
 
-def test_guest_cant_see_success_message(browser, link):
-    link = f"{link}"
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
     page = ProductPage(browser, link)
     page.open()
     page.should_not_be_success_message()
     
 @pytest.mark.xfail     
-def test_message_disappeared_after_adding_product_to_basket(browser, link):
-    link = f"{link}"
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
     page.should_be_disappeared_success_message()
+    
+        
+        
+        # pytest -v -s .\test_product_page.py
 
